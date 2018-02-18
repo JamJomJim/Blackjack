49e3b556e1c493d8a22f4bad065056e64bb28376
JamJomJim <thejamesewens@gmail.com>
2018-02-17 18:44:23 -0600

    Refactored cards to hand.

--- Blackjack.py
+++ Blackjack.py
@@ -21,41 +21,41 @@ class Card:
 
 class Deck:
     def __init__(self):
-        self.cards = []
+        self.hand = []
         for suit in suits:
             for rank in ranks:
-                self.cards.append(Card(suit, rank))
+                self.hand.append(Card(suit, rank))
 
     def displaycards(self):
-        print(self.cards)
+        print(self.hand)
 
     def shuffle(self):
-        random.shuffle(self.cards)
+        random.shuffle(self.hand)
 
 
 class Dealer:
     def __init__(self):
         self.deck1 = Deck()
         self.deck1.shuffle()
-        self.cards = []
+        self.hand = []
 
     def displaycards(self, start=None):
         if start is None:
-            print(self.cards)
+            print(self.hand)
         else:
-            print("Dealer has [" + str(self.cards[0]) + ", unknown]")
+            print("Dealer has [" + str(self.hand[0]) + ", unknown]")
 
     def deal(self, person, number):
-        person.cards = self.deck1.cards[0:number]
-        self.deck1.cards = self.deck1.cards[number:]
+        person.hand = self.deck1.hand[0:number]
+        self.deck1.hand = self.deck1.hand[number:]
 
 
 class Player:
     def __init__(self):
-        self.cards = []
+        self.hand = []
 
     def displaycards(self):
-        print("Player has", self.cards)
+        print("Player has", self.hand)
 
     def stand(self):
         temp = 1
@@ -66,24 +66,25 @@ class Player:
     def split(self):
         temp = 1
 
-def check_value(cards):
-    cards_sum = 0
-    for i in range(len(cards)):
-        if cards[i].rank is "ace":
-            cards_sum += 11
+
+def check_value(hand):
+    hand_sum = 0
+    for i in range(len(hand)):
+        if hand[i].rank is "ace":
+            hand_sum += 11
         else:
-            cards_sum += cards[i].rank
-        if cards[i].rank is "ace" and cards_sum > 21:
-            cards_sum -= 10
-    print(cards_sum)
-    return cards_sum
+            hand_sum += hand[i].rank
+        if hand[i].rank is "ace" and hand_sum > 21:
+            hand_sum -= 10
+    print(hand_sum)
+    return hand_sum
 
 dealer = Dealer()
 player = Player()
 dealer.deal(player, 2)
 dealer.deal(dealer, 2)
-check_value([Card("diamonds", "ace"), Card("diamonds", "ace")])
-
+print(player.hand)
+check_value(player.hand)
 
 
 
