c3d23085e2287a4173f4dcde25d3c65134e53825
JamJomJim <17938977+narvutar1324@users.noreply.github.com>
2018-02-21 02:50:06 -0600

    Testing commits

--- Blackjack.py
+++ Blackjack.py
@@ -303,7 +303,7 @@ def main():
         if len(dealer.deck.cards) / (number_of_decks * 52) < (1 - penetration):
             print("New Deck!")
             dealer.new_deck()
-            #resetting counts and suc
+            #resetting counts and such
             player.running_count, player.true_count, player.cards_seen, player.decks_remaining \
                 = 0, 0, 0, number_of_decks
 
