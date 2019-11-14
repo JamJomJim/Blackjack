c2613111fd183ad32d2f05d2134e4b10afa93361
James Ewens <jewens@adc4gis.com>
2019-11-14 10:38:38 -0600

    Code structure overhaul

--- Blackjack.py
+++ Blackjack.py
@@ -1,7 +1,6 @@
 import random
 import time
 import math
-import numpy
 
 # getters and setters would make things look nicer, just a refactoring thing, also its more proper
 # fix bets so that they're respond dynamically to counts
@@ -298,254 +297,16 @@ splitting_hand_strategy = \
             ['Y',     'Y',     'Y',    'Y',    'Y',    'N',    'N',    'N',    'N',     'N']]]
 
 
-def find_best_move(game, shoe, player, dealer, hand):
-    if dealer.hand[0][0].rank == "ace":
-        dealer_value = 11
-    else:
-        dealer_value = dealer.hand[0][0].rank
-    if player.hand[hand][0].rank == "ace":
-        player_value = 11
-    else:
-        player_value = player.hand[0][0].rank
-    try:
-        if len(player.hand[hand]) == 2 and player.hand[hand][0].rank == player.hand[hand][1].rank \
-                and splitting_hand_strategy[shoe.true_count + 3][abs(11 - player_value)][dealer_value - 2] == 'Y':
-            strat = 'split'
-        elif is_soft(player.hand[hand]):
-            player_value = check_value(player.hand[hand])
-            strat = soft_hand_strategy[shoe.true_count + 3][abs(20 - player_value)][dealer_value - 2]
-        else:
-            player_value = check_value(player.hand[hand])
-            strat = hard_hand_strategy[shoe.true_count + 3][abs(20 - player_value)][dealer_value - 2]
-
-        if strat != 'None':
-            return strat
-
-    except IndexError:
-        game.wrong_bs += 1
-        print("here")
-        return 'stand'
-
-
-class Game:
-    def __init__(self, blackjack_payout, dealer_hit_soft17, surrender, insurance, number_of_decks, penetration):
-        self.blackjack_payout = blackjack_payout
-        self.dealer_hit_soft17 = dealer_hit_soft17
-        self.surrender = surrender
-        self.insurance = insurance
-        self.number_of_decks = number_of_decks
-        self.penetration = penetration
-        # stats class eventually?
-        # for stat in stats print stat
-        self.current_round = 0
-        self.wrong_bs = 0
-        self.num_hits = 0
-        self.num_stand = 0
-        self.num_double = 0
-        self.num_split = 0
-        self.num_surrender = 0
-
-
-class Model:
-    def __init__(self, starting_amount, rounds_to_be_played, min_bet, is_manual):
-        self.starting_amount = starting_amount
-        self.rounds_to_be_played = rounds_to_be_played
-        self.min_bet = min_bet
-        self.is_manual = is_manual
-
-
-class Shoe:
-    def __init__(self, dealer, game):
-        self.cards_seen = 0
-        self.running_count = 0
-        self.true_count = 0
-        dealer.new_decks(game)
-
-
-def check_value(hand):
-    possible_values = [0]
-    # this essentially sets up a binomial tree with all the possible values.
-    for card in hand:
-        if card.rank == "ace":
-            temp_values = list(possible_values)
-            for value in possible_values:
-                temp_values.append(value + 1)
-                temp_values.append(value + 11)
-                temp_values.remove(value)
-            possible_values = temp_values
-        else:
-            for i, value in enumerate(possible_values):
-                possible_values[i] += card.rank
-    # lambda sets up an anonymous function that returns a bool.
-    # filter returns the values in possible_values that satisfy this function.
-    # these values are then turned into a list, and possible_values is set to that list.
-    possible_values = list(filter(lambda hand_value: hand_value <= 21, possible_values))
-    # the highest valid hand value is then returned.
-    # if the player is bust, return -1.
-    return max(possible_values + [-1])
-
-
-def is_soft(hand):
-    possible_values = [0]
-    for card in hand:
-        if card.rank == "ace":
-            temp_values = list(possible_values)
-            for value in possible_values:
-                temp_values.append(value + 1)
-                temp_values.append(value + 11)
-                temp_values.remove(value)
-            possible_values = temp_values
-        else:
-            for i, value in enumerate(possible_values):
-                possible_values[i] += card.rank
-    if len(list(filter(lambda hand_value: hand_value <= 21, possible_values))) > 1:
-        return True
-
-
-def count(dealer_hand, player_hand, shoe, game):
-
-    # this needs to be changed to work after every card dealt instead of a game to game basis
-
-    temp_hand = dealer_hand
-    for hand in player_hand:
-        temp_hand += hand
-
-    for card in temp_hand:
-        if card.rank != "ace":
-            if 2 <= int(card.rank) <= 6:
-                shoe.running_count += 1
-
-            elif 7 <= int(card.rank) <= 9:
-                shoe.running_count += 0
-
-            else:
-                shoe.running_count -= 1
-        else:
-            shoe.running_count -= 1
-
-    shoe.true_count = math.floor(shoe.running_count / (game.number_of_decks * 52 - shoe.cards_seen))
-
-
-class Card:
-    def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
-
-    def __str__(self):
-        return str(self.rank) + " of " + self.suit
-
-    def __repr__(self):
-        return str(self.rank) + " of " + self.suit
-
-    def displaysuit(self):
-        print(" ", self.suit)
-
-
-class Deck:
-    def __init__(self, game):
-        self.cards = []
-        for _ in range(game.number_of_decks):
-            for suit in suits:
-                for rank in ranks:
-                    self.cards.append(Card(suit, rank))
-
-    def display_cards(self):
-        print(self.cards)
-
-    def shuffle(self):
-        random.shuffle(self.cards)
-
-
-class Dealer:
-    def __init__(self):
-        self.deck = None
-        self.hand = [[]]
-
-    def displaycards(self, start=None):
-        if start is None:
-            print(self.hand)
-        else:
-            print("Dealer has [" + str(self.hand[0][0]) + ", unknown]")
-
-    def deal(self, person, hand, number_cards):
-        person.hand[hand] += self.deck.cards[0:number_cards]
-        self.deck.cards = self.deck.cards[number_cards:]
-
-    def hit(self, dealer, hand):
-        self.deal(dealer, hand, 1)
-
-    def stand(self):
-        pass
-
-    def clear_hand(self):
-        self.hand[0] = []
-
-    def new_decks(self, game):
-        self.deck = Deck(game)
-        self.deck.shuffle()
-
-
-class Player:
-    def __init__(self, model):
-        self.hand = [[]]
-        self.bankroll = model.starting_amount
-        self.bet = model.min_bet
-        self.current_bet = [0, 0, 0, 0, 0, 0, 0, 0]
-        self.total_bet = 0
-
-    def displaycards(self, i):
-        print("Player has", self.hand[i])
-
-    def hit(self, game, dealer, hand):
-        dealer.deal(self, hand, 1)
-        game.num_hits += 1
-
-    def double(self, game, dealer, hand):
-        self.place_bet(self.bet, hand)
-        dealer.deal(self, hand, 1)
-        game.num_double += 1
-
-    def split(self, game, dealer, i):
-        temp_hand = [self.hand[i][0]]
-        del self.hand[i][-1]
-        self.hand.append(temp_hand)
-        dealer.deal(self, -1, 1)
-        dealer.deal(self, i, 1)
-        self.place_bet(self.bet, i + 1)
-        game.num_split += 1
-
-    def surrender(self, game):
-        game.num_surrender += 1
-        self.stand(game)
-
-    def clear_hand(self):
-        self.hand = [[]]
-
-    def place_bet(self, amount, hand):
-        self.bankroll -= amount
-        self.current_bet[hand] += amount
-        self.total_bet += amount
-        # self.current_bet[hand] += amount
-
-    def show_bankroll(self):
-        print("We got", self.bankroll, "dollars boii!")
-
-    def choose_bet(self):
-        # fancy code resulting in self.bet being changed based off running or total count id remember
-        pass
-
-    def manipulate_bankroll(self, bet, hand):
-        self.bankroll += bet
-        print(self.current_bet[hand])
-        self.current_bet[hand] = 0
-
-
 def main():
     start = time.time()
-    # Game(blackjack_payout, dealer_hit_soft17, surrender, insurance, number_of_decks, penetration)
-    game = Game(1.5, True, True, True, 6, 0.75)
-    # Model(starting_amount, rounds_to_be_played, is_manual):
-    model = Model(0, 100000, 10, False)
+    game = Game(blackjack_payout=1.5,
+                dealer_hit_soft17=True,
+                surrender=True,
+                insurance=True,
+                number_of_decks=6,
+                penetration=0.75)
+
+    model = Model(starting_amount=0, rounds_to_be_played=100, min_bet=10, is_manual=False)
     dealer = Dealer()
     player = Player(model)
     shoe = Shoe(dealer, game)
@@ -690,6 +451,248 @@ def main():
     print("Surrender: " + str(game.num_surrender))
 
     print(player.bankroll/player.total_bet)
+
+
+class Game:
+    def __init__(self, blackjack_payout, dealer_hit_soft17, surrender, insurance, number_of_decks, penetration):
+        self.blackjack_payout = blackjack_payout
+        self.dealer_hit_soft17 = dealer_hit_soft17
+        self.surrender = surrender
+        self.insurance = insurance
+        self.number_of_decks = number_of_decks
+        self.penetration = penetration
+        # stats class eventually?
+        # for stat in stats print stat
+        self.current_round = 0
+        self.wrong_bs = 0
+        self.num_hits = 0
+        self.num_stand = 0
+        self.num_double = 0
+        self.num_split = 0
+        self.num_surrender = 0
+
+
+class Model:
+    def __init__(self, starting_amount, rounds_to_be_played, min_bet, is_manual):
+        self.starting_amount = starting_amount
+        self.rounds_to_be_played = rounds_to_be_played
+        self.min_bet = min_bet
+        self.is_manual = is_manual
+
+
+class Shoe:
+    def __init__(self, dealer, game):
+        self.cards_seen = 0
+        self.running_count = 0
+        self.true_count = 0
+        dealer.new_decks(game)
+
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return str(self.rank) + " of " + self.suit
+
+    def __repr__(self):
+        return str(self.rank) + " of " + self.suit
+
+    def displaysuit(self):
+        print(" ", self.suit)
+
+
+class Deck:
+    def __init__(self, game):
+        self.cards = []
+        for _ in range(game.number_of_decks):
+            for suit in suits:
+                for rank in ranks:
+                    self.cards.append(Card(suit, rank))
+
+    def display_cards(self):
+        print(self.cards)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+
+class Dealer:
+    def __init__(self):
+        self.deck = None
+        self.hand = []
+
+    def displaycards(self, start=None):
+        if start is None:
+            print(self.hand)
+        else:
+            print("Dealer has [" + str(self.hand[0][0]) + ", unknown]")
+
+    def deal(self, person, number_cards):
+        person.hand[hand] += self.deck.cards[0:number_cards]
+        self.deck.cards = self.deck.cards[number_cards:]
+
+    def hit(self, dealer, hand):
+        self.deal(person=dealer, number_cards=1)
+
+    def stand(self):
+        pass
+
+    def clear_hand(self):
+        self.hand[0] = []
+
+    def new_decks(self, game):
+        self.deck = Deck(game)
+        self.deck.shuffle()
+
+
+class Player:
+    def __init__(self, model):
+        self.hand = []
+        self.bankroll = model.starting_amount
+        self.bet = model.min_bet
+        self.current_bet = [0, 0, 0, 0, 0, 0, 0, 0]
+        self.total_bet = 0
+
+    def displaycards(self, i):
+        print("Player has", self.hand[i])
+
+    def hit(self, game, dealer, hand):
+        dealer.deal(self, hand, 1)
+        game.num_hits += 1
+
+    def double(self, game, dealer, hand):
+        self.place_bet(self.bet, hand)
+        dealer.deal(self, hand, 1)
+        game.num_double += 1
+
+    def split(self, game, dealer, i):
+        temp_hand = [self.hand[i][0]]
+        del self.hand[i][-1]
+        self.hand.append(temp_hand)
+        dealer.deal(self, -1, 1)
+        dealer.deal(self, i, 1)
+        self.place_bet(self.bet, i + 1)
+        game.num_split += 1
+
+    def surrender(self, game):
+        game.num_surrender += 1
+        self.stand(game)
+
+    def clear_hand(self):
+        self.hand = [[]]
+
+    def place_bet(self, amount, hand):
+        self.bankroll -= amount
+        self.current_bet[hand] += amount
+        self.total_bet += amount
+        # self.current_bet[hand] += amount
+
+    def show_bankroll(self):
+        print("We got", self.bankroll, "dollars boii!")
+
+    def choose_bet(self):
+        # fancy code resulting in self.bet being changed based off running or total count id remember
+        pass
+
+    def manipulate_bankroll(self, bet, hand):
+        self.bankroll += bet
+        print(self.current_bet[hand])
+        self.current_bet[hand] = 0
+
+
+def find_best_move(game, shoe, player, dealer, hand):
+    if dealer.hand[0][0].rank == "ace":
+        dealer_value = 11
+    else:
+        dealer_value = dealer.hand[0][0].rank
+    if player.hand[hand][0].rank == "ace":
+        player_value = 11
+    else:
+        player_value = player.hand[0][0].rank
+    try:
+        if len(player.hand[hand]) == 2 and player.hand[hand][0].rank == player.hand[hand][1].rank \
+                and splitting_hand_strategy[shoe.true_count + 3][abs(11 - player_value)][dealer_value - 2] == 'Y':
+            strat = 'split'
+        elif is_soft(player.hand[hand]):
+            player_value = check_value(player.hand[hand])
+            strat = soft_hand_strategy[shoe.true_count + 3][abs(20 - player_value)][dealer_value - 2]
+        else:
+            player_value = check_value(player.hand[hand])
+            strat = hard_hand_strategy[shoe.true_count + 3][abs(20 - player_value)][dealer_value - 2]
+
+        if strat != 'None':
+            return strat
+
+    except IndexError:
+        game.wrong_bs += 1
+        print("here")
+        return 'stand'
+
+
+def check_value(hand):
+    possible_values = [0]
+    # this essentially sets up a binomial tree with all the possible values.
+    for card in hand:
+        if card.rank == "ace":
+            temp_values = list(possible_values)
+            for value in possible_values:
+                temp_values.append(value + 1)
+                temp_values.append(value + 11)
+                temp_values.remove(value)
+            possible_values = temp_values
+        else:
+            for i, value in enumerate(possible_values):
+                possible_values[i] += card.rank
+    # lambda sets up an anonymous function that returns a bool.
+    # filter returns the values in possible_values that satisfy this function.
+    # these values are then turned into a list, and possible_values is set to that list.
+    possible_values = list(filter(lambda hand_value: hand_value <= 21, possible_values))
+    # the highest valid hand value is then returned.
+    # if the player is bust, return -1.
+    return max(possible_values + [-1])
+
+
+def is_soft(hand):
+    possible_values = [0]
+    for card in hand:
+        if card.rank == "ace":
+            temp_values = list(possible_values)
+            for value in possible_values:
+                temp_values.append(value + 1)
+                temp_values.append(value + 11)
+                temp_values.remove(value)
+            possible_values = temp_values
+        else:
+            for i, value in enumerate(possible_values):
+                possible_values[i] += card.rank
+    if len(list(filter(lambda hand_value: hand_value <= 21, possible_values))) > 1:
+        return True
+
+
+def count(dealer_hand, player_hand, shoe, game):
+
+    # this needs to be changed to work after every card dealt instead of a game to game basis
+
+    temp_hand = dealer_hand
+    for hand in player_hand:
+        temp_hand += hand
+
+    for card in temp_hand:
+        if card.rank != "ace":
+            if 2 <= int(card.rank) <= 6:
+                shoe.running_count += 1
+
+            elif 7 <= int(card.rank) <= 9:
+                shoe.running_count += 0
+
+            else:
+                shoe.running_count -= 1
+        else:
+            shoe.running_count -= 1
+
+    shoe.true_count = math.floor(shoe.running_count / (game.number_of_decks * 52 - shoe.cards_seen))
 
 
 if __name__ == '__main__':
