7ff8b3e216c2d97dad2711447500b32a001125af
JamJomJim <thejamesewens@gmail.com>
2020-07-24 19:46:42 -0500

    Unit tests for is_soft()

--- tests/test_hands.py
+++ tests/test_hands.py
@@ -3,10 +3,8 @@ from Main import Hand, Card
 
 
 class TestHands(unittest.TestCase):
-    def test_hard_hand(self):
+    def test_get_value_hard_hand(self):
         self.assertEqual(Hand(None, [Card("spades", 1), Card("spades", 2)], False, False, 0).get_value(), 3)
-
-    def test_soft_hand(self):
         self.assertEqual(Hand(None, [Card("spades", "ace"), Card("spades", 2)], False, False, 0).get_value(), 13)
         self.assertEqual(Hand(None, [Card("spades", "ace"), Card("spades", "ace")], False, False, 0).get_value(), 12)
         self.assertEqual(Hand(None, [Card("spades", "ace"), Card("spades", 10)], False, False, 0).get_value(), 21)
@@ -17,6 +15,24 @@ class TestHands(unittest.TestCase):
         self.assertEqual(
             Hand(None, [Card("spades", "ace"), Card("spades", "ace"), Card("spades", 1)], False, False, 0).get_value(),
             13
+        )
+
+    def test_is_soft(self):
+        self.assertEqual(Hand(None, [Card("spades", "ace"), Card("spades", 2)], False, False, 0).is_soft(), True)
+        self.assertEqual(Hand(None, [Card("spades", "ace"), Card("spades", 2)], False, False, 0).is_soft(), True)
+        self.assertEqual(
+            Hand(None,
+                 [Card("spades", "ace"),
+                  Card("spades", "ace"),
+                  Card("spades", "ace"),
+                  Card("spades", "ace"),
+                  Card("spades", "ace")],
+                 False, False, 0).is_soft(), True)
+        self.assertEqual(Hand(None, [Card("spades", 1), Card("spades", 2)], False, False, 0).is_soft(), False)
+        self.assertEqual(Hand(None, [Card("spades", 10), Card("spades", "ace")], False, False, 0).is_soft(), True)
+        self.assertEqual(
+            Hand(None, [Card("spades", 10), Card("spades", 5), Card("spades", "ace")], False, False, 0).is_soft(),
+            False
         )
 
 
